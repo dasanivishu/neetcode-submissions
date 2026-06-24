4cd201acdd3e4268d128b62ee58400fd9d84a3b6
Vishal Shyam Dasani <50577261+dasanivishu@users.noreply.github.com>
2026-06-24 09:37:11 +0530

    Add: anagram-groups - submission-16

---
+++ Data Structures & Algorithms/anagram-groups/submission-16.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>mp;
+        for(auto it:strs)
+        {
+            //first we will form key
+            vector<int>freq(26,0);
+            string key="";
+            for(auto ch:it)
+            {
+                freq[ch-'a']++;
+            }
+            for(auto fq:freq)
+            {
+                key+=fq;
+                key+=',';
+            }
+            mp[key].push_back(it);
+            
+        }
+        for(auto it:mp)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+        
+    }
+    //time complexity -O(m*n)
+    //space -O(m) where m is no of strings and n is max size of string
+};
